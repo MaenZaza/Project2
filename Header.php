18774bcd3c43e30b3985b0cc7a628f3af2e52d72
Shaham <mr.kaber97@gmail.com>
2023-11-06 12:54:22 +0100

    New code

--- Header.php
+++ Header.php
@@ -1,5 +1,16 @@
 <header>
 
-<button id="Logo"></button>
+    <button id="Logo"></button>
+
+    <button class="btn News">News</button>
+    <button class="btn Leden">Leden</button>
+    <button class="btn StandPunten">StandPunten</button>
+    <button class="btn Contact">Contact</button>
+    
+
     <button id="Doneren"></button>
 </header>
+<a href="">
+  <span>HOVER ME</span>
+  <div class="wave"></div>
+</a>
