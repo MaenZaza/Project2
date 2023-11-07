ee1463d954acaae53666792416d288529fbc0cab
Maen <maan2zaza3@gmail.com>
2023-11-07 12:58:57 +0100

    1

---
+++ Doneer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="test.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SP</title>
+</head>
+
+<body>
+<?php
+include "Header.php"
+?>
+<div class="Onderwerp">
+
+</div>
+
+
+
+
+
+<?php
+include "Footer.php"
+?>
+</body>
+</html>
