44af9f64343f713a39973a7b1336b66fdd7ae6d5
Maen <maan2zaza3@gmail.com>
2023-11-07 12:53:54 +0100

    pagina

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html lang="nl">
 <head>
     <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="test.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SP</title>
