25bef2f7ad4c2f360429693a6986cae45b5172b2
Shaham <mr.kaber97@gmail.com>
2023-11-07 12:54:07 +0100

    hrefs

--- Header.php
+++ Header.php
@@ -4,13 +4,13 @@
     <a id="Logo" href="index.php"></a>
 
   <div class="Menu">
-    <button class="btn News">News</button>
-    <button class="btn Leden">Leden</button>
-    <button class="btn StandPunten">StandPunten</button>
-    <button class="btn Contact">Contact</button>
+    <button href="News.php" class="btn News">News</button>
+    <button href="Leden.php" class="btn Leden">Leden</button>
+    <button href="StandPunten.php" class="btn StandPunten">StandPunten</button>
+    <button href="Contact.php" class="btn Contact">Contact</button>
   </div>
     
-    <a class="Doneer" href="">
+    <a class="Doneer" href="Doneer.php">
   <span>DONEER NU</span>
   <div class="wave"></div>
 </a>
