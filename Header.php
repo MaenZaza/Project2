a9ba4602968e5beeae4e608c7f0da6460bd709b6
Shaham <mr.kaber97@gmail.com>
2023-11-09 12:20:13 +0100

    new everything

--- Header.php
+++ Header.php
@@ -5,9 +5,9 @@
 
   <div class="Menu">
     <a href="Nieuws.php" class="btn NieuwsM">Nieuws</a>
-    <a href="Leden.php" class="btn Leden">Leden</a>
-    <a href="StandPunten.php" class="btn StandPunten">StandPunten</a>
-    <a href="Contact.php" class="btn Contact">Contact</a>
+    <a href="Leden.php" class="btn LedenM">Leden</a>
+    <a href="StandPunten.php" class="btn StandPuntenM">StandPunten</a>
+    <a href="Contact.php" class="btn ContactM">Contact</a>
   </div>
     
     <a class="Doneer" href="Doneer.php">
