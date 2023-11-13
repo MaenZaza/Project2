1d7e2606e71f9e3fa0ead6c61afcaf1d4bba2c59
Shaham <mr.kaber97@gmail.com>
2023-11-13 11:55:53 +0100

    fotos met informatie

--- Leden.php
+++ Leden.php
@@ -17,16 +17,26 @@
 include "Header.php"
 ?>
 <div id="LedenPage" class="Onderwerp">
-    <div class="Ledenp L1">1. LILIAN MARIJNISSEN <br>Oss, 1985 - Lijsttrekker en huidig fractievoorzitter SP Tweede Kamerfractie</div>
-    <div class="Ledenp L2">2</div>
-    <div class="Ledenp L3">3</div>
-    <div class="Ledenp L4">4</div>
-    <div class="Ledenp L5">5</div>
-    <div class="Ledenp L6">6</div>
-    <div class="Ledenp L7">7</div>
-    <div class="Ledenp L8">8</div>
-    <div class="Ledenp L9">9</div>
-    <div class="Ledenp L10">10</div>
+    <div class="Ledenp L1">1. LILIAN MARIJNISSEN <br>
+    Oss, 1985 - Lijsttrekker en huidig fractievoorzitter SP Tweede Kamerfractie</div>
+    <div class="Ledenp L2">2. SANDRA BECKERMAN <br>
+    Groningen, 1983 - SP-Kamerlid, woordvoerder Groningen, Volkshuisvesting, Natuur en Landbouw</div>
+    <div class="Ledenp L3">3. BART VAN KENT<br>
+    Den Haag, 1983 - SP-Kamerlid, woordvoerder Sociale Zaken en pensioenen</div>
+    <div class="Ledenp L4">4. JIMMY DIJK<br>
+    Groningen, 1985 - SP-Kamerlid, woordvoerder Zorg</div>
+    <div class="Ledenp L5">5. MICHIEL VAN NISPEN<br>
+    Breda, 1982 - SP-Kamerlid, woordvoerder Justitie en Veiligheid</div>
+    <div class="Ledenp L6">6. SARAH DOBBE<br>
+    Arnhem, 1979 - Oud-organiser voor de FNV en medewerker van de SP Tweede Kamerfractie</div>
+    <div class="Ledenp L7">7. NICOLE TEMMINK<br>
+    Zwolle, 1987 - SP-Kamerlid Binnenlandse Zaken en Koninkrijksrelaties</div>
+    <div class="Ledenp L8">8. MATHIJS TEN BROEKE<br>
+    Zutphen, 1990 - Medewerker Woonbond, voormalig fractievoorzitter en wethouder in Zutphen</div>
+    <div class="Ledenp L9">9. JASPER VAN DIJK<br>
+    Utrecht, 1971 - SP-Kamerlid Buitenlandse Zaken, Defensie en Asiel</div>
+    <div class="Ledenp L10">10. BASTIAAN MEIJER<br>
+    Den Haag, 1997 - Voorzitter SP Jongeren, voormalig voorzitter FNV Jong, fractiemedewerker Sociale Zaken</div>
 
 </div>
 
