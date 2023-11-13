2c73506d1dc13e8638cb6a3d953b0c851042c1d9
Maen <maan2zaza3@gmail.com>
2023-11-13 10:02:38 +0100

    c

--- Contact.php
+++ Contact.php
@@ -17,8 +17,35 @@
 include "Header.php"
 ?>
 <div id="ContactPage" class="Onderwerp">
-    <div class="Form FR"></div>
-    <div class="Form FL"></div>
+    <div class="Form FR">
+    <div class="form-group">
+    <label for="vraag">Vraag</label>
+    <textarea name="vraag" id="vraag" required></textarea>
+  </div>
+    </div>
+    
+    <div class="Form FL">
+    <form action="mail.php" method="post">
+  <div class="form-group">
+    <label for="naam">Naam</label>
+    <input type="text" name="naam" id="naam" required>
+  </div>
+  <div class="form-group">
+    <label for="achternaam">Achternaam</label>
+    <input type="text" name="achternaam" id="achternaam" required>
+  </div>
+  <div class="form-group">
+    <label for="telefoonnummer">Telefoonnummer</label>
+    <input type="tel" name="telefoonnummer" id="telefoonnummer" required>
+  </div>
+  <div class="form-group">
+    <label for="email">E-mailadres</label>
+    <input type="email" name="email" id="email" required>
+  </div>
+  
+  <input type="submit" value="Verzenden">
+</form>
+    </div>
     
 
 </div>
