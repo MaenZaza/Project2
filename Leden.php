a670f618b24d1dd5c6ff0ce96111669a16b56dd5
Maen <maan2zaza3@gmail.com>
2023-11-14 10:50:25 +0100

    leden

--- Leden.php
+++ Leden.php
@@ -17,36 +17,76 @@
 include "Header.php"
 ?>
 <div id="LedenPage" class="Onderwerp">
-    <div class="Ledenp L1" ><img src="Images/Leden1.jpg" alt="Leed1"><br>
-        1. LILIAN MARIJNISSEN <br>
-    Oss, 1985 - Lijsttrekker en huidig fractievoorzitter SP Tweede Kamerfractie</div>
-    <div class="Ledenp L2"><img src="Images/Leden2.jpg" alt="Leed1"><br>
-    2. SANDRA BECKERMAN <br>
-    Groningen, 1983 - SP-Kamerlid, woordvoerder Groningen, Volkshuisvesting, Natuur en Landbouw</div>
-    <div class="Ledenp L3"><img src="Images/Leden3.jpg" alt="Leed1"><br>
-    3. BART VAN KENT<br>
-    Den Haag, 1983 - SP-Kamerlid, woordvoerder Sociale Zaken en pensioenen</div>
-    <div class="Ledenp L4"><img src="Images/Leden4.jpg" alt="Leed1"><br>
-    4. JIMMY DIJK<br>
-    Groningen, 1985 - SP-Kamerlid, woordvoerder Zorg</div>
-    <div class="Ledenp L5"><img src="Images/Leden5.jpg" alt="Leed1"><br>
-    5. MICHIEL VAN NISPEN<br>
-    Breda, 1982 - SP-Kamerlid, woordvoerder Justitie en Veiligheid</div>
-    <div class="Ledenp L6"><img src="Images/Leden6.jpg" alt="Leed1"><br>
-    6. SARAH DOBBE<br>
-    Arnhem, 1979 - Oud-organiser voor de FNV en medewerker van de SP Tweede Kamerfractie</div>
-    <div class="Ledenp L7"><img src="Images/Leden7.jpg" alt="Leed1"><br>
-    7. NICOLE TEMMINK<br>
-    Zwolle, 1987 - SP-Kamerlid Binnenlandse Zaken en Koninkrijksrelaties</div>
-    <div class="Ledenp L8"><img src="Images/Leden8.jpg" alt="Leed1"><br>
-    8. MATHIJS TEN BROEKE<br>
-    Zutphen, 1990 - Medewerker Woonbond, voormalig fractievoorzitter en wethouder in Zutphen</div>
-    <div class="Ledenp L9"><img src="Images/Leden9.jpg" alt="Leed1"><br>
-    9. JASPER VAN DIJK<br>
-    Utrecht, 1971 - SP-Kamerlid Buitenlandse Zaken, Defensie en Asiel</div>
-    <div class="Ledenp L10"><img src="Images/Leden10.jpg" alt="Leed1"><br>
-    10. BASTIAAN MEIJER<br>
-    Den Haag, 1997 - Voorzitter SP Jongeren, voormalig voorzitter FNV Jong, fractiemedewerker Sociale Zaken</div>
+    <div class="Ledenp L1" >
+        <img src="Images/Leden1.jpg" alt="Leed1">
+        <div>
+            1. LILIAN MARIJNISSEN
+            Oss, 1985 - Lijsttrekker en huidig fractievoorzitter SP Tweede Kamerfractie
+        </div>    
+    </div>
+    <div class="Ledenp L2">
+        <img src="Images/Leden2.jpg" alt="Leed1">
+        <div>
+            2. SANDRA BECKERMAN         
+            Groningen, 1983 - SP-Kamerlid, woordvoerder Groningen, Volkshuisvesting, Natuur en Landbouw
+        </div>
+    </div>
+    <div class="Ledenp L3">
+        <img src="Images/Leden3.jpg" alt="Leed1">
+        <div>
+            3. BART VAN KENT
+            Den Haag, 1983 - SP-Kamerlid, woordvoerder Sociale Zaken en pensioenen
+            </div>
+    </div>
+    <div class="Ledenp L4">
+        <img src="Images/Leden4.jpg" alt="Leed1">
+        <div>
+            4. JIMMY DIJK
+            Groningen, 1985 - SP-Kamerlid, woordvoerder Zorg
+        </div>
+    </div>
+    <div class="Ledenp L5">
+        <img src="Images/Leden5.jpg" alt="Leed1">
+        <div>
+            5. MICHIEL VAN NISPEN
+            Breda, 1982 - SP-Kamerlid, woordvoerder Justitie en Veiligheid
+            </div>
+    </div>
+    <div class="Ledenp L6">
+        <img src="Images/Leden6.jpg" alt="Leed1">
+        <div>
+            6. SARAH DOBBE
+            Arnhem, 1979 - Oud-organiser voor de FNV en medewerker van de SP Tweede Kamerfractie
+         </div>
+    </div>
+    <div class="Ledenp L7">
+        <img src="Images/Leden7.jpg" alt="Leed1">
+        <div>
+            7. NICOLE TEMMINK
+             Zwolle, 1987 - SP-Kamerlid Binnenlandse Zaken en Koninkrijksrelaties
+        </div>
+    </div>
+    <div class="Ledenp L8">
+        <img src="Images/Leden8.jpg" alt="Leed1">
+        <div>
+            8. MATHIJS TEN BROEKE
+            Zutphen, 1990 - Medewerker Woonbond, voormalig fractievoorzitter en wethouder in Zutphen
+       </div>
+    </div>
+    <div class="Ledenp L9">
+        <img src="Images/Leden9.jpg" alt="Leed1">
+        <div>
+            9. JASPER VAN DIJK
+            Utrecht, 1971 - SP-Kamerlid Buitenlandse Zaken, Defensie en Asiel
+          </div>
+    </div>
+    <div class="Ledenp L10">
+        <img src="Images/Leden10.jpg" alt="Leed1">
+        <div>
+            10. BASTIAAN MEIJER
+            Den Haag, 1997 - Voorzitter SP Jongeren, voormalig voorzitter FNV Jong, fractiemedewerker Sociale Zaken
+        </div>
+    </div>
 
 </div>
 
