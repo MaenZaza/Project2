c79df5ef3d279462f0144c44e37be74948915544
Maen <maan2zaza3@gmail.com>
2023-11-17 11:01:59 +0100

    1

--- dbHandler.php
+++ dbHandler.php
@@ -2,7 +2,7 @@
 
 class DbHandler
 {
-    private $dataSource = "mysql:dbname=sp;host=localhost;port=3307";
+    private $dataSource = "mysql:dbname=sp;host=localhost;";
 
     private $userName = "root";
     private $password = "";
